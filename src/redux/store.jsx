73f3d3c660eb44470d375582ead6baaf69370e27
Emmanuel Benjamin D <emmanuel26112000@gmail.com>
2024-01-26 22:27:30 +0530

    food slice commented in all place

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,13 +2,13 @@ import { configureStore } from "@reduxjs/toolkit"
 import tokenReducer from './tokenSlice'
 import orderDataReducer  from "./orderDataSlice"
 import getDataReducer from "./getDataSlice"
-import foodsReducer from "./foodsSlice"
+// import foodsReducer from "./foodsSlice"
 
 export default configureStore({
     reducer: {
         token: tokenReducer,
         orderData: orderDataReducer,
         getData: getDataReducer,
-        foods:foodsReducer
+        // foods:foodsReducer
     }
 })
